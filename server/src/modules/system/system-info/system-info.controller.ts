a326f5ed61d5c2e5c461167bae261d1a7863de47
王国帅 <2388838628@qq.com>
2024-10-26 23:45:59 +0800

    feat(system-info): system-info module add swagger

--- server/src/modules/system/system-info/system-info.controller.ts
+++ server/src/modules/system/system-info/system-info.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { SystemInfoService } from './system-info.service';
 import { Public } from 'src/common/decorator/public/public.decorator';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+@ApiTags('系统信息')
 @Controller('systemInfo')
 export class SystemInfoController {
   constructor(private readonly systemInfoService: SystemInfoService) {}
+  @ApiOperation({ summary: '获取服务器信息' })
   @Public()
   @Get('all')
   findAll() {
