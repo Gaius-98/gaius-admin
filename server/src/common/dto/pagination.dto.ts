832dab3350613f138b69a18d71598f9c72aba055
王国帅 <2388838628@qq.com>
2024-10-26 23:43:58 +0800

    feat(global): import nestjs/swagger

--- server/src/common/dto/pagination.dto.ts
+++ server/src/common/dto/pagination.dto.ts
@@ -1,5 +1,15 @@
-export default class PaginationDto {
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+export class PaginationDto {
+  @ApiProperty({ default: 1 })
   pageNumber: number;
+  @ApiProperty({ default: 10 })
   pageSize: number;
+  @ApiPropertyOptional()
   keyword?: string;
+}
+export class ResListDto<T> {
+  @ApiProperty({ isArray: true })
+  data: T[];
+  @ApiProperty()
+  total: number;
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filter/http-exception/http-exception.filter';
-// import { TransformInterceptor } from './common/interceptor/transform/transform.interceptor';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   const configSrv = app.get(ConfigService);
   app.use(cookieParser());
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('gaius-admin')
+    .setDescription('gaius-admin service api')
+    .setVersion('1.0')
+    .addTag('gaius-admin')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
   const port = configSrv.get('serve.port') || 3000;
   app.useStaticAssets(join(__dirname, '../', 'static'), {
     prefix: '/static',
